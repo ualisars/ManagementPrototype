a22d780f6b43025772112c2a98adfe27b5f3496e
Ualikhan Sar <ualikhansars@gmail.com>
2024-04-22 23:35:43 +0500

    add disable unit

--- Characters/character.gd
+++ Characters/character.gd
@@ -5,6 +5,7 @@ var defense: int
 var health: int
 var character_name: String
 var is_player_controlled: bool
+var id: int
 
 var player_color: String = "5e4086"
 var enemy_color: String = "fc4086"
@@ -17,8 +18,11 @@ func init(characteristics: Node, player_controlled: bool) -> void:
 	health = characteristics.health
 	character_name = characteristics.character_name
 	is_player_controlled = player_controlled
+	id = characteristics.id
 
 func _ready() -> void:
+	Messenger.UNIT_ATTACKED.connect(on_unit_attacked)
+	
 	var material: StandardMaterial3D = StandardMaterial3D.new()
 	
 	if is_player_controlled:
@@ -27,3 +31,10 @@ func _ready() -> void:
 		material.albedo_color = enemy_color
 		
 	body.material_override = material
+	
+func on_unit_attacked(attack_unit: Node, defend_unit: Node) -> void:
+	if defend_unit.id == id and defend_unit.health <= 0:
+		disable_unit()
+		
+func disable_unit() -> void:
+	body.rotate_z(90.0)
