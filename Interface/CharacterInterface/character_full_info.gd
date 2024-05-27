03b13ddd15081fb740542ebb582f341e4caab985
Ualikhan Sar <ualikhansars@gmail.com>
2024-05-27 17:39:09 +0500

    add general info

--- Interface/CharacterInterface/character_full_info.gd
+++ Interface/CharacterInterface/character_full_info.gd
@@ -1,9 +1,10 @@
 extends Control
 
-@onready var attack_value: Label = $PlayerCharacteristicsContainer/AttackValue
-@onready var defense_value: Label = $PlayerCharacteristicsContainer/DefenseValue
-@onready var health_value: Label = $PlayerCharacteristicsContainer/HealthValue
-@onready var concentration_value: Label = $PlayerCharacteristicsContainer/ConcentrationValue
+@onready var attack_value: Label = $CharacteristicsContainer/AttackValue
+@onready var defense_value: Label = $CharacteristicsContainer/DefenseValue
+@onready var health_value: Label = $CharacteristicsContainer/HealthValue
+@onready var concentration_value: Label = $CharacteristicsContainer/ConcentrationValue
+@onready var character_name: Label = $GeneralInfoContainer/CharacterName
 
 func _ready() -> void:
 	Messenger.CHARACTER_INFO_CHOSEN.connect(on_character_info_chosen)
@@ -13,3 +14,5 @@ func on_character_info_chosen(characteristics: Node) -> void:
 	defense_value.text = str(characteristics.defense)
 	health_value.text = str(characteristics.health)
 	concentration_value.text = str(characteristics.concentration)
+	
+	character_name.text = characteristics.character_name
