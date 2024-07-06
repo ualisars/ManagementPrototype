f534682858144d3b0ab46a21fbf5d774235a24ad
ualisars <ualikhansars@gmail.com>
2024-07-07 03:45:36 +0600

    add experience to character full info

--- Interface/CharacterInterface/character_full_info.gd
+++ Interface/CharacterInterface/character_full_info.gd
@@ -8,6 +8,10 @@ extends Control
 
 @onready var learnt_spell_container: GridContainer = $CharacterInfoContainer/LearntSpellContainer
 
+@onready var level_progress_bar = $CharacterInfoContainer/LevelProgressBar
+@onready var level_progress_bar_value = $CharacterInfoContainer/LevelProgressBar/LevelProgressBarValue
+@onready var level_value = $CharacterInfoContainer/CharacteristicsContainer/LevelValue
+
 var LearntSpellIconClass: PackedScene = preload("res://Interface/CharacterInterface/learnt_spell_icon.tscn")
 
 func _ready() -> void:
@@ -24,13 +28,29 @@ func on_character_info_chosen(characteristics: Node) -> void:
 	
 	init_learnt_spell_container()
 	
+	display_level(characteristics)
+	
 func on_spell_learnt(spell: Node):
 	var spell_icon = LearntSpellIconClass.instantiate()
 	spell_icon.texture = spell.spell_texture
 		
 	learnt_spell_container.add_child(spell_icon)
 	
-func init_learnt_spell_container():
+func display_level(characteristics: Characterictic) -> void:
+	var level: int = characteristics.level
+	var next_level: int = level + 1
+	
+	var experience_to_next_level: int = LevelManager.get_level_points(next_level)
+	var experience: int = characteristics.experience
+	
+	level_value.text = str(level)
+	
+	level_progress_bar.max_value = experience_to_next_level
+	level_progress_bar.value = characteristics.experience
+	
+	level_progress_bar_value.text = str(experience) + "/" + str(experience_to_next_level)
+	
+func init_learnt_spell_container() -> void:
 	for learnt_spell in learnt_spell_container.get_children():
 		learnt_spell_container.remove_child(learnt_spell)
 		learnt_spell.queue_free()
