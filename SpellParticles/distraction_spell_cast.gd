f1c87d96a0c85d538a6e017139e50767d8fcb85b
ualisars <ualikhansars@gmail.com>
2024-08-01 13:51:14 +0600

    change domination spell to use spell3d

--- SpellParticles/distraction_spell_cast.gd
+++ SpellParticles/distraction_spell_cast.gd
@@ -3,9 +3,9 @@ extends Spell3D
 @onready var distraction_spell_cast_particle = $DistractionSpellCastParticle
 
 func _ready():
+	super()
+	
 	material = distraction_spell_cast_particle.material_override
 	material.set_shader_parameter("TransitionAmount", 0.0)
 	
 	total_duration = cast_usage_speed + cast_time
-	
-	super()
--- SpellParticles/domination_cast_spell.gd
+++ SpellParticles/domination_cast_spell.gd
@@ -2,38 +2,10 @@ extends Spell3D
 
 @onready var gpu_particles_3d = $GPUParticles3D
 
-#var transition_amount
-#
-#var elapsed_time: float = 0.0
-#
-#var material: ShaderMaterial
-#
-#var total_duration: float
-
-
 func _ready():
+	super()
+	
 	material = gpu_particles_3d.material_override
 	material.set_shader_parameter("TransitionAmount", 0.0)
 	
 	total_duration = cast_usage_speed + cast_time
-
-
-func _process(delta):
-	elapsed_time += delta
-
-	transition_amount = lerp(0.0, 1.0, elapsed_time / total_duration)
-
-	transition_amount = clamp(transition_amount, 0.0, 1.0)
-
-	if transition_amount >= 1.0:
-		for effect in spell.effects:
-			Messenger.SPELL_EFFECTS_APPLIED.emit(
-				owner_character,
-				target_character,
-				self
-			)
-		
-		set_process(false)
-		queue_free()
-
-	material.set_shader_parameter("TransitionAmount", transition_amount)
